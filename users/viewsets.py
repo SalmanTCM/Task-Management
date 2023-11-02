dd6dd64a91357cc617fabd154b2492ee5124be5c
SalmanTCM <salmann.hossain@gmail.com>
2023-11-02 23:45:13 +0600

    user auth with restapi

---
+++ users/viewsets.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .permissions import IsUserOwnerOrGetAndPostOnly
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsUserOwnerOrGetAndPostOnly,]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # def list(self, request):
+    #     queryset = User.objects.all()
+    #     serializer_class = UserSerializer
